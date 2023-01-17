13ecd5fb7dc80e573cb7f42bac4307a301d89045
Cody MACEDO <cody.m@kiwip.tech>
2023-01-17 11:55:45 +0100

    qrcode project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/alt-text */
+import QRCode from 'qrcode';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [url, setUrl] = useState('')
+  const [color, setColor] = useState('#000000')
+  const [qrcode, setQrCode] = useState('')
+
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(url,{
+      width: 500,
+      margin: 1,
+      color: {
+        dark: color,
+        light: "#ffffff00"
+      }
+    }, (err, url) => {
+      if (err) return console.log(err)
+      console.log(url)
+      setQrCode(url)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>QR Code Generator</h1>
+      <div className="input-section">
+        <input className="input" onChange={(ev) => setUrl(ev.target.value)} value={url} type="text" placeholder='http://www.kiwip.eu' />
+        <input className="input" onChange={(ev) => setColor(ev.target.value)} value={color} type="text" placeholder='#000000' />
+        <button className="btn" onClick={GenerateQRCode} >Créer le QrCode</button>
+      </div>
+      {qrcode &&
+        <div className="qrcode-section">
+          <img src={qrcode} />
+          <a className="btn" href={qrcode} download="qrcode.png"> Télécharger </a>
+        </div>}
+
     </div>
   );
 }
